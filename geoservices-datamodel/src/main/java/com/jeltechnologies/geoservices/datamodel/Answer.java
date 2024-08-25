3adea6237cd4aa2e5aeeede16629fbb3c07e28b7
jelte <jelte@jeltechnologies.com>
2024-08-25 20:15:06 +0200

    Improved answer

--- geoservices-datamodel/src/main/java/com/jeltechnologies/geoservices/datamodel/Answer.java
+++ geoservices-datamodel/src/main/java/com/jeltechnologies/geoservices/datamodel/Answer.java
@@ -1,5 +1,7 @@
 package com.jeltechnologies.geoservices.datamodel;
 
+import java.util.Objects;
+
 public class Answer {
     private Address address = new Address();
     private Distance distanceFromQuery;
@@ -28,9 +30,28 @@ public class Answer {
     public Coordinates getClosestCoordinate() {
 	return closestCoordinate;
     }
-
+    
     @Override
     public String toString() {
 	return "Answer [address=" + address + ", distanceFromQuery=" + distanceFromQuery + ", closestCoordinate=" + closestCoordinate + "]";
+    }
+    
+
+    @Override
+    public int hashCode() {
+	return Objects.hash(address, closestCoordinate, distanceFromQuery);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Answer other = (Answer) obj;
+	return Objects.equals(address, other.address) && Objects.equals(closestCoordinate, other.closestCoordinate)
+		&& Objects.equals(distanceFromQuery, other.distanceFromQuery);
     }
 }
